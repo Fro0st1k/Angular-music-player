ffa870a2537942393f12b6c71ace77c87d4c8f8b
Maksim Belkin <Maksim_Belkin@epam.com>
2018-06-26 14:42:22 +0400

    added song mocks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RoutingModule } from './routing/routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutingModule } from './routing/routing.module';
 
 import { AppComponent } from './components/app/app.component';
 import { PlayerBarComponent } from './components/player-bar/player-bar.component';
